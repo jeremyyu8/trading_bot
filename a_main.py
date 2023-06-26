e36c7df020c3b0b40aadd4ff25734932f3466436
Jeremy Yu <jeremyyu8@gmail.com>
2023-06-26 00:13:16 -0500

    remove pycache

--- a_main.py
+++ a_main.py
@@ -3,7 +3,10 @@ from a_pnl_tracker import PNLTracker
 
 from a_strategy import MovingAvgStrategy, RSIStrategy
 
-OKX_data_manager = OkxDataManager(symbols=["BTC-USDT", "LTC-USDT", "ETH-USDT"], types=["live", "live", "live"])
+#OKX_data_manager = OkxDataManager(symbols=["BTC-USDT", "LTC-USDT", "ETH-USDT"], types=["live", "live", "live"])
+
+OKX_data_manager = OkxDataManager(symbols=["BTC-USDT"], types=["live"])
+
 PNL_tracker = PNLTracker(initial_balance=1000000)
 
 OKX_data_manager.start()
