2c0d9f70dfbf29c759cf5df75236bc8f1514de2a
Jeremy Yu <jeremyyu8@gmail.com>
2023-07-10 01:55:35 -0500

    plotting feature upon close

--- portfolio_manager.py
+++ portfolio_manager.py
@@ -1,17 +1,23 @@
 import threading
+from pynput import keyboard
+import matplotlib.pyplot as plt
+from datetime import datetime 
+
 class PortfolioManager():
     '''
     Manages risk among all strategies, tracks positions and PNL over all strategies and exchanges
     '''
 
     def __init__(self, initial_balance, risk_manager, equities) -> None:
-        self.net_positions = {key: value for key, value in zip(equities, [0 for i in range(len(equities))])}
-        self.prices = {key: value for key, value in zip(equities, [0 for i in range(len(equities))])}
+        self.net_positions = {equity: 0 for equity in equities}
+        self.prices = {equity: 0 for equity in equities}
         self.initial_balance = initial_balance
         self.balance = initial_balance 
         self.risk_manager = risk_manager
-        self.pnls = []
+        self.pnls_over_time = []
+        self.net_position_values_over_time = {equity: [] for equity in equities}
         self.portfolio_lock = threading.Lock()
+        self.start_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     #Constant Proportion Portfolio Insurance (CPPI)
     def cppi_risk_manager(self, max_loss_percent = 0.1, max_asset_downside = 0.2):
@@ -44,7 +50,7 @@ class PortfolioManager():
             risk_limit = balance_limit
         risk_limit-=self.net_positions[symbol]
 
-        print(size, balance_limit, risk_limit)
+        # print(size, balance_limit, risk_limit)
         return(min(size, min(balance_limit, risk_limit)))
 
     def buy(self, price, size, symbol):
@@ -81,7 +87,7 @@ class PortfolioManager():
             risk_limit = 0
         risk_limit-=(-self.net_positions[symbol])
 
-        print(size, risk_limit)
+        # print(size, risk_limit)
         return(min(size, risk_limit))
     
     def sell(self, price, size, symbol, fixed=None):
@@ -114,7 +120,7 @@ class PortfolioManager():
         #update price for pnl calculation
         self.prices[symbol] = price
 
-        print("Starting rebalance")
+        print("Starting portfolio rebalance")
         risk_limit = None
         if self.risk_manager == "cppi":
             risk_limit = self.cppi_risk_manager()/price
@@ -123,7 +129,7 @@ class PortfolioManager():
         elif self.risk_manager == "ratio":
             risk_limit = self.ratio_risk_manager()/price
 
-        print(risk_limit)
+        # print(risk_limit)
         
         if risk_limit:
             if self.net_positions[symbol] > risk_limit:
@@ -141,11 +147,28 @@ class PortfolioManager():
     def get_pnl(self):
         asset_value = 0
         for key in self.net_positions:
-            asset_value += self.net_positions[key] * self.prices[key]
+            cur_equity_value = self.net_positions[key] * self.prices[key]
+            self.net_position_values_over_time[key].append(cur_equity_value)
+            asset_value += cur_equity_value
 
         pnl = (self.balance + asset_value) - self.initial_balance
-        self.pnls.append(pnl)
+        self.pnls_over_time.append(pnl)
         return pnl
+    
     
     def get_available_balance(self):
         return self.balance
+
+    def plot(self):
+        finish_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        plt.figure(figsize=(12,6))
+        plt.plot(self.pnls_over_time, linewidth=2, label="PNL")
+        for equity in self.net_position_values_over_time:
+            # print(self.net_position_values_over_time[equity])
+            plt.plot(self.net_position_values_over_time[equity], label = equity)
+
+        plt.xlabel('time')
+        plt.ylabel('USD')
+        plt.title(f'Plot of PNLS and Net Positions from {self.start_time} to {finish_time}')
+        plt.legend()
+        plt.show()
